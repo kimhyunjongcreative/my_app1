1df7ef41921af48be974fd33ca743849495612f1
kimhyunjongcreative <ttliu@nate.com>
2016-12-21 12:41:47 +0900

    change1

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ var app = express();
 app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',function(req,res){
-  res.render('my_first_ejs');
+var data={count:0};
+app.get('/', function (req,res){
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+app.get('/reset',function(req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count',function(req,res){
+  if(req.query.count)data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/:num',function(req,res){
+  data.count=req.params.num;
+  req.render('my_first_ejs',data);
 });
 
 app.listen(3000,function(){
