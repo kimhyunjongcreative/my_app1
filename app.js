a239178567e15e49516335883cfe1cfd2ec7199f
kimhyunjongcreative <ttliu@nate.com>
2016-12-21 12:09:10 +0900

    change1

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',function(req,res){
+  res.render('my_first_ejs');
+});
 
 app.listen(3000,function(){
   console.log('Server Oh!');
